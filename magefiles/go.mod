df28bbbeecb36234c811e3299f26a7213641e5e1
laupse <lgrondin@ingeniance.fr>
2023-01-18 15:26:34 +0100

    multi-platform and conc test

---
+++ magefiles/go.mod
@@ -0,0 +1,31 @@
+module github.com/inpulse-tv/dagger-demo/magefiles
+
+go 1.19
+
+require (
+	dagger.io/dagger v0.4.3
+	github.com/sourcegraph/conc v0.2.0
+	go.uber.org/multierr v1.9.0
+)
+
+require (
+	github.com/Khan/genqlient v0.5.0 // indirect
+	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/cockroachdb/errors v1.9.0 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20211118104740-dabe8e521a4f // indirect
+	github.com/cockroachdb/redact v1.1.3 // indirect
+	github.com/getsentry/sentry-go v0.13.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/iancoleman/strcase v0.2.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	github.com/sourcegraph/sourcegraph/lib v0.0.0-20221216004406-749998a2ac74 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.1 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
+	golang.org/x/sys v0.0.0-20220829200755-d48e67d00261 // indirect
+)
+
+replace github.com/docker/docker => github.com/docker/docker v20.10.3-0.20220414164044-61404de7df1a+incompatible
---
+++ magefiles/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"dagger.io/dagger"
+	"github.com/sourcegraph/conc/pool"
+	"go.uber.org/multierr"
+)
+
+var platforms = []dagger.Platform{
+	"linux/amd64", // a.k.a. x86_64
+	"linux/arm64", // a.k.a. aarch64
+	// "linux/s390x", // a.k.a. IBM S/390
+}
+
+func Engine() error {
+	ctx := context.Background()
+
+	client, err := dagger.Connect(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	return nil
+}
+
+func runUnitTest(client *dagger.Client, workdir *dagger.Directory, ctx context.Context, platform dagger.Platform) error {
+	_, err := client.
+		Container(dagger.ContainerOpts{Platform: platform}).
+		From("golang:1.19.3-alpine").
+		WithDirectory("/go/src/dagger-demo", workdir).
+		WithWorkdir("/go/src/dagger-demo").
+		WithExec([]string{"/usr/local/go/bin/go", "test", "./math", "-v"}).
+		Stdout(ctx)
+	return err
+}
+
+func Testconcurrent() error {
+	ctx := context.Background()
+
+	client, err := dagger.Connect(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+	p := pool.NewWithResults[error]()
+
+	workdir := client.
+		Host().
+		Directory(".", dagger.HostDirectoryOpts{
+			Exclude: []string{"magefiles", "go.work"},
+		})
+
+	start := time.Now()
+	for _, platform := range platforms {
+		p.Go(func() error {
+			return runUnitTest(client, workdir, ctx, platform)
+		})
+	}
+	res := p.Wait()
+	fmt.Printf("%d\n", time.Since(start).Milliseconds())
+	var errors error
+	for _, e := range res {
+		if e != nil {
+			return multierr.Append(errors, e)
+		}
+	}
+
+	return errors
+}
+
+func Test() error {
+	ctx := context.Background()
+
+	client, err := dagger.Connect(ctx)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	workdir := client.
+		Host().
+		Directory(".", dagger.HostDirectoryOpts{
+			Exclude: []string{"magefiles", "go.work"},
+		})
+
+	var errors error
+	start := time.Now()
+	for _, platform := range platforms {
+		err := runUnitTest(client, workdir, ctx, platform)
+		if err != nil {
+			return multierr.Append(errors, err)
+		}
+	}
+	fmt.Printf("%d\n", time.Since(start).Milliseconds())
+	return errors
+}
+
+func innerBuild(client *dagger.Client) []*dagger.Container {
+	workdir := client.Host().
+		Directory(".", dagger.HostDirectoryOpts{Exclude: []string{"magefiles"}})
+	containers := []*dagger.Container{}
+	for _, platform := range platforms {
+		containers = append(containers, client.
+			Container(dagger.ContainerOpts{Platform: platform}).
+			From("golang:1.19.3-alpine").
+			WithDirectory("/go/src/dagger-demo", workdir).
+			WithWorkdir("/go/src/dagger-demo").
+			WithEnvVariable("CGO_ENABLED", "0").
+			WithExec([]string{"/usr/local/go/bin/go", "build", "-o", "./output/dagger-demo"}))
+	}
+	return containers
+}
+
+func Build() error {
+	ctx := context.Background()
+
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		return err
+	}
+
+	for _, container := range innerBuild(client) {
+		container.Stdout(ctx)
+	}
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Push() error {
+	ctx := context.Background()
+
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		return err
+	}
+
+	platformVariants := []*dagger.Container{}
+	for i, container := range innerBuild(client) {
+		output := container.Directory("./output/")
+		platformVariants = append(platformVariants, client.Container(dagger.ContainerOpts{Platform: platforms[i]}).WithRootfs(output))
+	}
+
+	fmt.Println(len(platformVariants))
+	_, err = client.
+		Container().
+		Export(ctx, "dagger-demo.tar",
+			dagger.ContainerExportOpts{PlatformVariants: platformVariants},
+		)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
